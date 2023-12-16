44f8bf569421bec4e3fee5aa9efd39dda8d66e95
blasbenito <blasbenito@gmail.com>
2023-12-16 08:12:22 +0100

    updated the file psi.h

--- src/psi.h
+++ src/psi.h
@@ -3,41 +3,66 @@
 
 #include <Rcpp.h>
 
-double psi_paired_cpp(
-        Rcpp::NumericMatrix a,
-        Rcpp::NumericMatrix b,
-        const std::string& method = "euclidean"
+// Forward declarations of functions
+Rcpp::DataFrame psi_cost_path_cpp(
+    Rcpp::NumericMatrix a,
+    Rcpp::NumericMatrix b,
+    const std::string& method = "euclidean",
+    bool diagonal = false,
+    bool weighted = false,
+    bool ignore_blocks = false
 );
 
-double psi_cpp(
-        Rcpp::NumericMatrix a,
-        Rcpp::NumericMatrix b,
-        const std::string& method = "euclidean",
-        bool diagonal = false,
-        bool weighted = false,
-        bool ignore_blocks = false
+double psi_auto_sum_cpp(
+    Rcpp::NumericMatrix a,
+    Rcpp::NumericMatrix b,
+    Rcpp::DataFrame path,
+    const std::string& method = "euclidean",
+    bool ignore_blocks = false
+);
+
+double psi_formula_cpp(
+    Rcpp::DataFrame path,
+    double auto_sum,
+    bool diagonal = false
+);
+
+double psi_paired_cpp(
+    Rcpp::NumericMatrix a,
+    Rcpp::NumericMatrix b,
+    const std::string& method = "euclidean"
 );
 
 Rcpp::NumericVector null_psi_paired_cpp(
-        Rcpp::NumericMatrix a,
-        Rcpp::NumericMatrix b,
-        const std::string& method = "euclidean",
-        Rcpp::IntegerVector block_size = Rcpp::IntegerVector::create(2, 3, 4),
-        int seed = 1,
-        int repetitions = 100
+    Rcpp::NumericMatrix a,
+    Rcpp::NumericMatrix b,
+    const std::string& method = "euclidean",
+    Rcpp::IntegerVector block_size = Rcpp::IntegerVector::create(2, 3, 4),
+    int seed = 1,
+    bool independent_columns = true,
+    int repetitions = 100
+);
+
+double psi_cpp(
+    Rcpp::NumericMatrix a,
+    Rcpp::NumericMatrix b,
+    const std::string& method = "euclidean",
+    bool diagonal = false,
+    bool weighted = false,
+    bool ignore_blocks = false
 );
 
 Rcpp::NumericVector null_psi_cpp(
-        Rcpp::NumericMatrix a,
-        Rcpp::NumericMatrix b,
-        const std::string& method = "euclidean",
-        bool diagonal = false,
-        bool weighted = false,
-        bool ignore_blocks = false,
-        Rcpp::IntegerVector block_size = Rcpp::IntegerVector::create(2, 3, 4),
-        int seed = 1,
-        int repetitions = 100
+    Rcpp::NumericMatrix a,
+    Rcpp::NumericMatrix b,
+    const std::string& method = "euclidean",
+    bool diagonal = false,
+    bool weighted = false,
+    bool ignore_blocks = false,
+    Rcpp::IntegerVector block_size = Rcpp::IntegerVector::create(2, 3, 4),
+    int seed = 1,
+    bool independent_columns = true,
+    int repetitions = 100
 );
 
-#endif // PSI_H
-
+#endif  // PSI_H
