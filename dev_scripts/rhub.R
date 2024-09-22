45ca20aae62cda7925de60596f2f865c8ee00cc4
blasbenito <blasbenito@gmail.com>
2024-09-22 09:58:05 +0200

    - missed a couple of files

---
+++ dev_scripts/rhub.R
@@ -0,0 +1,33 @@
+install.packages("rhub")
+Sys.setenv(GITHUB_PAT = "my_pat")
+rhub::rhub_setup()
+rhub::rhub_doctor()
+
+#full check
+rhub::rhub_check(
+  platforms = c(
+    "linux",
+    "macos",
+    "macos-arm64",
+    "windows",
+    "atlas",
+    "c23",
+    "clang-asan",
+    "clang16",
+    "clang17",
+    "clang18",
+    "clang19",
+    "gcc13",
+    "gcc14",
+    "intel",
+    "mkl",
+    "nold",
+    "nosuggests",
+    "rchk",
+    "ubuntu-clang",
+    "ubuntu-gcc12",
+    "ubuntu-next",
+    "ubuntu-release",
+    "valgrind"
+  )
+)
